97e462071c0e7b033a327dae289a580978e85d35
JakubJaskulki <jtjaskulski@outlook.com>
2026-03-04 16:18:21 +0100

    feat(Lecture2TailWind): init changes

--- Company/Company.PortalWWW/Controllers/HomeController.cs
+++ Company/Company.PortalWWW/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace Company.PortalWWW.Controllers
         public IActionResult Contact() => View();
 
         public IActionResult About() => View();
+        public IActionResult Products() => View();
+        public IActionResult StyleGuide() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
