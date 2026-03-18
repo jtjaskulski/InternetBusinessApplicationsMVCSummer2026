4ab805b72c875ea9cfc098944408ad5b94a2cc9a
JakubJaskulki <jtjaskulski@outlook.com>
2026-03-18 16:40:13 +0100

    feat(Lecture4): add Category, Product, Page, and News entities with validation attributes

---
+++ Company/Company.Data/Data/CompanyContext.cs
@@ -0,0 +1,19 @@
+﻿using Company.Data.Data.CMS;
+using Company.Data.Data.Shop;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Data.Data
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext (DbContextOptions<CompanyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Page> Page { get; set; } = default!;
+        public DbSet<News> News { get; set; } = default!;
+        public DbSet<Product> Product { get; set; } = default!;
+        public DbSet<Category> Category { get; set; } = default!;
+    }
+}
