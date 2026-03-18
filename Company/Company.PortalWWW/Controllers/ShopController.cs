ee8762fe14ad5e51262fb5ec0b9a315e3c5efa9d
JakubJaskulki <jtjaskulski@outlook.com>
2026-03-18 18:03:46 +0100

    feat: add layout and footer components for improved site structure

---
+++ Company/Company.PortalWWW/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Company.Data.Data;
+using Company.PortalWWW.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PortalWWW.Controllers
+{
+    public class ShopController : Controller
+    {
+        private ILogger<ShopController> _logger;
+        private CompanyContext _context;
+
+        public ShopController(ILogger<ShopController> logger, CompanyContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index() => View();
+
+        public IActionResult Contact() => View();
+
+        public IActionResult About() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
