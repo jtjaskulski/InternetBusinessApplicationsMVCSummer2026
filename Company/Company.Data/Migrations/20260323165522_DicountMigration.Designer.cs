a2510bd378aee3536f57cdb4446a915dd85e21c2
JakubJaskulki <jtjaskulski@outlook.com>
2026-03-23 18:08:36 +0100

    feat: add IsDiscount property to Product model and implement Discounts view

--- Company/Company.Data/Migrations/20260323165522_DicountMigration.Designer.cs
+++ Company/Company.Data/Migrations/20260323165522_DicountMigration.Designer.cs
@@ -3,6 +3,7 @@ using Company.Data.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Company.Data.Migrations
 {
     [DbContext(typeof(CompanyContext))]
-    partial class CompanyContextModelSnapshot : ModelSnapshot
+    [Migration("20260323165522_DicountMigration")]
+    partial class DicountMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Company/Company.Data/Migrations/20260323165522_DicountMigration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Company.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DicountMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDiscount",
+                table: "Product",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "IdProduct",
+                keyValue: 1,
+                column: "IsDiscount",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "IdProduct",
+                keyValue: 2,
+                column: "IsDiscount",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "IdProduct",
+                keyValue: 3,
+                column: "IsDiscount",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "IdProduct",
+                keyValue: 4,
+                column: "IsDiscount",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "IdProduct",
+                keyValue: 5,
+                column: "IsDiscount",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDiscount",
+                table: "Product");
+        }
+    }
+}
